2eac5d5d523c7f0c6dcc66265642bb14352133c1
TheStarBoys <shengzang19980@gmail.com>
2023-12-20 11:06:32 +0800

    Add .vscode and update comemnts in flatten.sh

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -1,14 +1,23 @@
 #! /bin/bash
-function read_dir(){
-    for file in `ls $1`       #注意此处这是两个反引号，表示运行系统命令
+
+#This function reads a directory and calls the forge flatten command to flatten the contract
+function flatten_all(){
+    #Loop through each file in the directory
+    for file in `ls $1`
     do
-        if [ -d $1"/"$file ]  #注意此处之间一定要加上空格，否则会报错
+        #Check if the file is a directory
+        if [ -d $1"/"$file ]
         then
-            read_dir $1"/"$file
+            #If the file is a directory, call the flatten_all function again
+            flatten_all $1"/"$file
         else
-            file_path=$1"/"$file   #在此处处理文件即可
+            #If the file is not a directory, store the file path
+            file_path=$1"/"$file
+            #Store the flatten file path
             flatten_file_path=$flatten_dir"/"$file
+            #Print out the flatten command
             echo flatten $file_path to $flatten_file_path
+            #Call the forge flatten command
             forge flatten $file_path > $flatten_file_path
         fi
     done
@@ -17,9 +26,13 @@ function read_dir(){
 root=`pwd`
 contracts_dir=$root/contracts/src
 flatten_dir=$root/flatten
+
 echo flatten contracts in $contracts_dir
-#读取第一个参数
+
+#Remove the flatten directory if it exists
 rm -rf $flatten_dir
+#Create the flatten directory
 mkdir $flatten_dir
 
-read_dir $contracts_dir
+#Call the read_dir function to read the contracts directory
+flatten_all $contracts_dir
