09300fbba58a9512b904f8c6b0438a2dcb45be13
TheStarBoys <shengzang19980@gmail.com>
2024-05-15 10:36:59 +0800

    update settings

--- scripts/report_bugs.sh
+++ scripts/report_bugs.sh
@@ -5,6 +5,6 @@ contract_root="$1"
 #     contract_root=`pwd`
 rm -rf report/slither
 mkdir -p report/slither
-slither $contract_root --exclude-dependencies --exclude-informational --exclude-low --exclude-medium > report/slither/high.md --checklist
-slither $contract_root --exclude-dependencies --exclude-informational --exclude-low --exclude-high > report/slither/medium.md --checklist
-slither $contract_root --exclude-dependencies --exclude-informational --exclude-medium --exclude-high > report/slither/low.md --checklist
+slither $contract_root --exclude-dependencies --exclude-informational --exclude-low --exclude-medium > report/slither/high.md --checklist --compile-force-framework foundry --foundry-out-directory cache/forge/out
+slither $contract_root --exclude-dependencies --exclude-informational --exclude-low --exclude-high > report/slither/medium.md --checklist --compile-force-framework foundry --foundry-out-directory cache/forge/out
+slither $contract_root --exclude-dependencies --exclude-informational --exclude-medium --exclude-high > report/slither/low.md --checklist --compile-force-framework foundry --foundry-out-directory cache/forge/out
